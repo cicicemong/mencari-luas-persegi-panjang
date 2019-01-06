63ebb684c67fe9d2f6d5cb10cd3523786f794bd6
cici cemong <45526554+cicicemong@users.noreply.github.com>
2019-01-06 11:14:58 +0700

    Add files via upload

---
+++ menghitung luas persegi panjang.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include"conio.h"
+using namespace std;
+int main()
+{
+    int p,l;
+    cout<<"PENGHITUNG LUAS PERSEGI PANJANG"<<endl;
+    cout<<"panjang=";
+    cin>>p;
+    cout<<"lebar=";
+    cin>>l;
+    cout<<"Luas Persegi Panjang=";cout<<(p*l);
+    getch();
+}
